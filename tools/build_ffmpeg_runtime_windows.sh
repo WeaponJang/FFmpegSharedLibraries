eb67c4d7d026ba2ced9933b480c4306db86d80c0
WeaponJang <104521645+WeaponJang@users.noreply.github.com>
2026-06-17 11:44:55 +0800

    Update build_ffmpeg_runtime_windows.sh
    去掉jxl编码器支持

--- tools/build_ffmpeg_runtime_windows.sh
+++ tools/build_ffmpeg_runtime_windows.sh
@@ -374,14 +374,13 @@ CONFIGURE_FLAGS=(
   --enable-libx265
   --enable-libvpx
   --enable-libwebp
-  --enable-libjxl
   --enable-libtwolame
   --enable-libopus
   --disable-indevs
   --disable-outdevs
   --disable-devices
   --disable-encoders
-  --enable-encoder=png,apng,libx264,libx265,ffv1,flv,libjxl,libjxl_anim,qoi,libvpx-vp9,libwebp_anim,libwebp,yuv4,eac3,libtwolame,qoa,pcm_s16le
+  --enable-encoder=png,apng,libx264,libx265,ffv1,flv,qoi,libvpx-vp9,libwebp_anim,libwebp,yuv4,eac3,libtwolame,qoa,pcm_s16le
   --extra-ldflags="-static"
   --pkg-config-flags="--static"
   "--extra-libs=-Wl,-Bstatic -lwinpthread -lstdc++ -lpthread -lwinmm -Wl,-Bdynamic"
